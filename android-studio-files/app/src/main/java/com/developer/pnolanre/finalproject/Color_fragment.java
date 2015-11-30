5f24150ee442ad9ebc493c04b93babdc966a2203
unknown <pnolanre@pnolanre-MOBL.amr.corp.intel.com>
2015-11-30 13:11:09 -0800

    Enabling data to be saved through rotation

--- android-studio-files/app/src/main/java/com/developer/pnolanre/finalproject/Color_fragment.java
+++ android-studio-files/app/src/main/java/com/developer/pnolanre/finalproject/Color_fragment.java
@@ -23,9 +23,13 @@ public class Color_fragment extends Fragment {
     public static final int DEFAULT_GREEN = 0;
     public static final int DEFAULT_BLUE = 125;
     public static final int NUM_PATTERNS = 6;
+    public static final String KEY_RED="red";
+    public static final String KEY_GREEN="green";
+    public static final String KEY_BLUE="blue";
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, final Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_color, parent, false);
         pCustomization = (CustomizationInterface) getActivity();
         red_bar = (SeekBar) v.findViewById(R.id.red_bar);
@@ -45,6 +49,9 @@ public class Color_fragment extends Fragment {
 
             @Override
             public void onProgressChanged(SeekBar red_bar, int progress, boolean userInput) {
+                if (savedInstanceState != null){
+                    red_value = savedInstanceState.getInt(KEY_RED,0);
+                }
                 red_value = progress;
             }
 
@@ -58,6 +65,9 @@ public class Color_fragment extends Fragment {
 
             @Override
             public void onProgressChanged(SeekBar green_bar, int progress, boolean userInput) {
+                if (savedInstanceState != null){
+                    green_value = savedInstanceState.getInt(KEY_GREEN,0);
+                }
                 green_value = progress;
             }
 
@@ -71,6 +81,9 @@ public class Color_fragment extends Fragment {
 
             @Override
             public void onProgressChanged(SeekBar blue_bar, int progress, boolean userInput) {
+                if (savedInstanceState != null){
+                    blue_value = savedInstanceState.getInt(KEY_BLUE,0);
+                }
                 blue_value = progress;
             }
 
@@ -81,6 +94,13 @@ public class Color_fragment extends Fragment {
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
         return v;
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(KEY_RED, red_value);
+        savedInstanceState.putInt(KEY_GREEN, green_value);
+        savedInstanceState.putInt(KEY_BLUE, blue_value);
     }
 
     public void sendColor() {
